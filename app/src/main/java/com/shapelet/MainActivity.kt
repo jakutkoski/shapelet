09c6f9d5bd35bb2aa91915c9bd9fbdb9a240920e
jakutkoski <jakutkoski@gmail.com>
2024-06-25 17:44:44 -0500

    got line drawing kinda working

--- app/src/main/java/com/shapelet/MainActivity.kt
+++ app/src/main/java/com/shapelet/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -26,14 +27,19 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.LayoutCoordinates
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.positionInRoot
 import androidx.compose.ui.unit.dp
 import com.shapelet.ui.theme.ShapeletTheme
 
-// TODO draw lines
+// TODO prettify line drawing, dotted lines for un-submitted, etc.
 // TODO check words against dictionary
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,6 +59,18 @@ class MainActivity : ComponentActivity() {
 @Composable
 private fun Board() {
     var activatedOrder by rememberSaveable { mutableStateOf(listOf<Int>()) }
+    var node0Position by remember { mutableStateOf(Offset.Zero) }
+    var node1Position by remember { mutableStateOf(Offset.Zero) }
+    var node2Position by remember { mutableStateOf(Offset.Zero) }
+    var node3Position by remember { mutableStateOf(Offset.Zero) }
+    var node4Position by remember { mutableStateOf(Offset.Zero) }
+    var node5Position by remember { mutableStateOf(Offset.Zero) }
+    var node6Position by remember { mutableStateOf(Offset.Zero) }
+    var node7Position by remember { mutableStateOf(Offset.Zero) }
+    var node8Position by remember { mutableStateOf(Offset.Zero) }
+    var node9Position by remember { mutableStateOf(Offset.Zero) }
+    var node10Position by remember { mutableStateOf(Offset.Zero) }
+    var node11Position by remember { mutableStateOf(Offset.Zero) }
 
     val letterString = "BSAGKOZRUEIT"
     val letters = letterString.toList().map { it.toString() }
@@ -61,6 +79,44 @@ private fun Board() {
         return {
             if (activatedOrder.isEmpty() || activatedOrder.last() !in nodeSide) {
                 activatedOrder = activatedOrder.toMutableList().apply { add(nodeNumber) }.toList()
+            }
+        }
+    }
+
+    fun getNodePosition(nodeNumber: Int): Offset {
+        return when(nodeNumber) {
+            0 -> node0Position
+            1 -> node1Position
+            2 -> node2Position
+            3 -> node3Position
+            4 -> node4Position
+            5 -> node5Position
+            6 -> node6Position
+            7 -> node7Position
+            8 -> node8Position
+            9 -> node9Position
+            10 -> node10Position
+            11 -> node11Position
+            else -> Offset.Zero
+        }
+    }
+
+    Canvas(modifier = Modifier.fillMaxSize()) {
+        if (activatedOrder.size >= 2) {
+            var i = 0
+            while (i + 1 < activatedOrder.size) {
+                val firstNode = activatedOrder[i]
+                val secondNode = activatedOrder[i+1]
+                if (firstNode != Constants.SUBMIT && secondNode != Constants.SUBMIT) {
+                    val firstNodePosition = getNodePosition(firstNode)
+                    val secondNodePosition = getNodePosition(secondNode)
+                    drawLine(
+                        start = firstNodePosition,
+                        end = secondNodePosition,
+                        color = Color.Black
+                    )
+                }
+                i++
             }
         }
     }
@@ -83,9 +139,15 @@ private fun Board() {
             .padding(start = 30.dp, end = 30.dp),
             horizontalArrangement = Arrangement.SpaceEvenly
         ) {
-            Node(0, letters[0], activatedOrder, getOnClick(0, setOf(0,1,2)))
-            Node(1, letters[1], activatedOrder, getOnClick(1, setOf(0,1,2)))
-            Node(2, letters[2], activatedOrder, getOnClick(2, setOf(0,1,2)))
+            Node(0, letters[0], activatedOrder, getOnClick(0, setOf(0,1,2))) {
+                node0Position = it.positionInRoot()
+            }
+            Node(1, letters[1], activatedOrder, getOnClick(1, setOf(0,1,2))) {
+                node1Position = it.positionInRoot()
+            }
+            Node(2, letters[2], activatedOrder, getOnClick(2, setOf(0,1,2))) {
+                node2Position = it.positionInRoot()
+            }
         }
         Spacer(modifier = Modifier.size(30.dp))
         Row(modifier = Modifier
@@ -93,8 +155,12 @@ private fun Board() {
             .padding(start = 10.dp, end = 10.dp),
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
-            Node(3, letters[3], activatedOrder, getOnClick(3, setOf(3,4,5)))
-            Node(6, letters[6], activatedOrder, getOnClick(6, setOf(6,7,8)))
+            Node(3, letters[3], activatedOrder, getOnClick(3, setOf(3,4,5))) {
+                node3Position = it.positionInRoot()
+            }
+            Node(6, letters[6], activatedOrder, getOnClick(6, setOf(6,7,8))) {
+                node6Position = it.positionInRoot()
+            }
         }
         Spacer(modifier = Modifier.size(30.dp))
         Row(modifier = Modifier
@@ -102,8 +168,12 @@ private fun Board() {
             .padding(start = 10.dp, end = 10.dp),
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
-            Node(4, letters[4], activatedOrder, getOnClick(4, setOf(3,4,5)))
-            Node(7, letters[7], activatedOrder, getOnClick(7, setOf(6,7,8)))
+            Node(4, letters[4], activatedOrder, getOnClick(4, setOf(3,4,5))) {
+                node4Position = it.positionInRoot()
+            }
+            Node(7, letters[7], activatedOrder, getOnClick(7, setOf(6,7,8))) {
+                node7Position = it.positionInRoot()
+            }
         }
         Spacer(modifier = Modifier.size(30.dp))
         Row(modifier = Modifier
@@ -111,8 +181,12 @@ private fun Board() {
             .padding(start = 10.dp, end = 10.dp),
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
-            Node(5, letters[5], activatedOrder, getOnClick(5, setOf(3,4,5)))
-            Node(8, letters[8], activatedOrder, getOnClick(8, setOf(6,7,8)))
+            Node(5, letters[5], activatedOrder, getOnClick(5, setOf(3,4,5))) {
+                node5Position = it.positionInRoot()
+            }
+            Node(8, letters[8], activatedOrder, getOnClick(8, setOf(6,7,8))) {
+                node8Position = it.positionInRoot()
+            }
         }
         Spacer(modifier = Modifier.size(30.dp))
         Row(modifier = Modifier
@@ -120,17 +194,32 @@ private fun Board() {
             .padding(start = 30.dp, end = 30.dp),
             horizontalArrangement = Arrangement.SpaceEvenly
         ) {
-            Node(9, letters[9], activatedOrder, getOnClick(9, setOf(9,10,11)))
-            Node(10, letters[10], activatedOrder, getOnClick(10, setOf(9,10,11)))
-            Node(11, letters[11], activatedOrder, getOnClick(11, setOf(9,10,11)))
+            Node(9, letters[9], activatedOrder, getOnClick(9, setOf(9,10,11))) {
+                node9Position = it.positionInRoot()
+            }
+            Node(10, letters[10], activatedOrder, getOnClick(10, setOf(9,10,11))) {
+                node10Position = it.positionInRoot()
+            }
+            Node(11, letters[11], activatedOrder, getOnClick(11, setOf(9,10,11))) {
+                node11Position = it.positionInRoot()
+            }
         }
         Spacer(modifier = Modifier.size(80.dp))
         Row(modifier = Modifier
             .fillMaxWidth(),
             horizontalArrangement = Arrangement.Center
         ) {
-            Button(onClick = {
-                if (activatedOrder.isNotEmpty()) activatedOrder = activatedOrder.dropLast(1)
+            Button(onClick = onClick@{
+                if (activatedOrder.isEmpty()) return@onClick
+                if (activatedOrder.size <= 3) {
+                    activatedOrder = activatedOrder.dropLast(1)
+                    return@onClick
+                }
+                if (activatedOrder.lastIndexOf(Constants.SUBMIT) == activatedOrder.size - 2) {
+                    activatedOrder = activatedOrder.dropLast(2)
+                    return@onClick
+                }
+                activatedOrder = activatedOrder.dropLast(1)
             }) {
                 Text(text = "Delete")
             }
@@ -160,7 +249,13 @@ private fun Board() {
 }
 
 @Composable
-private fun Node(id: Int, letter: String, activatedOrder: List<Int>, onClick: () -> Unit) {
+private fun Node(
+    id: Int,
+    letter: String,
+    activatedOrder: List<Int>,
+    onClick: () -> Unit,
+    onGloballyPositioned: (LayoutCoordinates) -> Unit
+) {
     val activated = id in activatedOrder
     val lastActivated = activatedOrder.isNotEmpty() && activatedOrder.last() == id
 
@@ -176,7 +271,9 @@ private fun Node(id: Int, letter: String, activatedOrder: List<Int>, onClick: ()
 
     OutlinedButton(
         shape = RoundedCornerShape(CornerSize(0)),
-        modifier = Modifier.size(45.dp),
+        modifier = Modifier
+            .size(45.dp)
+            .onGloballyPositioned(onGloballyPositioned),
         contentPadding = PaddingValues(0.dp),
         colors = ButtonDefaults.buttonColors(
             containerColor = Color.White,
